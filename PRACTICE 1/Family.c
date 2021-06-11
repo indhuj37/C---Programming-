5c18eee7e4dda4c1cc73bf5e01045424ab978eaa
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-11 21:43:49 +0530

    Add files via upload

---
+++ PRACTICE 1/Family.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+main()
+{
+    printf("\nMy name is Indhu");
+    printf("\nMy Father name is Jagadeesan");
+    printf("\nMy Mother name is sankari");
+}
---
+++ PRACTICE 1/student details.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+printf("My name is Indhu");
+printf("I am studying B.E Computer Science ");
+printf("My CGPA is 65%");
+}
