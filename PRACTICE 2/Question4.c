c6096ca652a8b63e171df30bccbcac3dea74f028
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-16 21:47:45 +0530

    Add files via upload

---
+++ PRACTICE 2/Question4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    char ename[20];
+    int salary;
+    printf("\nEnter your employee name:");
+    scanf("\n%s",ename);
+    printf("\nEnter youremployee salary:");
+    scanf("\n%d",&salary);
+}
