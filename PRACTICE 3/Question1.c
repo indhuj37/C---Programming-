6bc1e63e9ee8770e1dcd7a983b0ef20955186e00
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-16 22:12:44 +0530

    Add files via upload

---
+++ PRACTICE 3/Question1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int a;
+    int b;
+    int c;
+    printf("\nEnter the value for a:");
+    scanf("\n%d",&a);
+    printf("\nEnter the value for b:");
+    scanf("\n%d",&b);
+    c=a+b;
+    printf("\nThe addition of a and b is %d",c);
+}
