bed99614196885727f41ba36b5e34f5cf43f7196
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-16 21:31:03 +0530

    Add files via upload

---
+++ PRACTICE 2/Question3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    char name[20];
+    int age;
+    printf("\nEnter your name:");
+    scanf("\n%s",name);
+    printf("\nEnter your age:");
+    scanf("\n%d",&age);
+}
