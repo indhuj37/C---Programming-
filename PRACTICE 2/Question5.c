78c264ed398aa4676b531af37cc9868f04a01b11
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-16 22:00:00 +0530

    Add files via upload

---
+++ PRACTICE 2/Question5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    char sname[20];
+    int rollno;
+    printf("\nEnter your student name:");
+    scanf("\n%s",sname);
+    printf("\nEnter your student rollno:");
+    scanf("\n%d",&rollno);
+}
