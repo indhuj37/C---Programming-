fe27e30142c17c3cb8d141853f701df57bd7cba1
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-17 22:13:19 +0530

    Add files via upload

---
+++ PRACTICE 4/Question1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+main()
+{
+    int a;
+    int b;
+    printf("\nEnter the value for a");
+    scanf("\n%d",&a);
+    printf("\nEnter the value for b");
+    scanf("\n%d",&b);
+    if(a>b)
+    printf("\nthe value of a is greater");
+    else
+    printf("\nthe value of b is greater");
+}
+    
+    
