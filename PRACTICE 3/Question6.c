2e490c5160bcf7d854aa4f7761609744446a5cdd
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-17 21:48:16 +0530

    Add files via upload

---
+++ PRACTICE 3/Question6.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    printf("\nEnter the value for a:");
+    scanf("\n%d",&a);
+    printf("\nEnter the value for b:");
+    scanf("\n%d",&b);
+    printf("\nEnter the value for c:");
+    scanf("\n%d",&c);
+    d=(a+b+c)/3;
+    printf("\nThe average of a,b and c is %d",d);
+}
