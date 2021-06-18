9baa36cedc192755047a63d50b06c7ba72a920c9
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-18 22:12:57 +0530

    Add files via upload

---
+++ PRACTICE 4/Question4.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+main()
+{   int num;
+    printf("\nEnter the number:");
+    scanf("\n%d",&num);
+    if(num>0)
+    printf("\nThe number is positive");
+    else
+    printf("\nThe number is negative");
+}
+
