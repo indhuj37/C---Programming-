13600e43e89c21f5629351c1fb2cfa85a216f390
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-18 21:34:18 +0530

    Add files via upload

---
+++ PRACTICE 4/Question2.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+main()
+{
+    int a;
+    int b;
+    printf("\nEnter the value for a");
+    scanf("\n%d",&a);
+    printf("\nEnter the value for b");
+    scanf("\n%d",&b);
+    if(a<b)
+    printf("\n the value for a is lesser");
+    else
+    printf("\n the value for b is lesser");
+}
+
