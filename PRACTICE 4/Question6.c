e74e1398a94f569715c9bad0451f71c6c48e271f
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-18 22:50:43 +0530

    Add files via upload

---
+++ PRACTICE 4/Question6.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+main()
+{
+    int int1,int2;
+    printf("\nInput the values of Number1 & Number2:");
+    scanf("\n%d%d",&int1,&int2);
+    if(int1==int2)
+    printf("\nNumber1 and Number2 is equal");
+    else
+    printf("\nNumber1 and Number2 are not equal");
+}
