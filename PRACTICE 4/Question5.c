6e26ec1814d06547d400dd8578fd62a79af9204e
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-18 22:36:20 +0530

    Add files via upload

---
+++ PRACTICE 4/Question5.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+main()
+{
+    int num;
+    printf("\nCheck whether even or odd:");
+    scanf("\n%d",&num);
+    if(num%2==0)
+    printf("\n%d is even",num);
+    else
+    printf("\n%d is odd",num);
+}
