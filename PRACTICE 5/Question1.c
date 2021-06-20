1afd51b0a0d46f0bc843a8278c5844af52a1efe5
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-20 19:37:09 +0530

    Add files via upload

---
+++ PRACTICE 5/Question1.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+main()
+{
+    int a;
+    int b;
+    int c;
+    printf("\n Enter the value for a");
+    scanf("\n%d",&a);
+    printf("\nEnter the value for b");
+    scanf("\n%d",&b);
+    printf("\nEnter the value for c");
+    scanf("\n%d",&c);
+    if((a>b)&&(a>c))
+    printf("\nthe value for a is greater");
+    else if((b>a)&&(b>c))
+    printf("\nthe value for b is greater");
+    else
+    printf("\nthe value for c is greater");
+}
