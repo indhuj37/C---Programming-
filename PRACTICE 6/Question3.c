82fe0ff4bce9fa2bab691a4f8df7e263673c1120
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-22 22:18:12 +0530

    Add files via upload

---
+++ PRACTICE 6/Question3.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+main()
+{
+    char vowel;
+    printf("\nEnter the vowel");
+    scanf("\n%c",&vowel);
+    if(vowel=='a')
+    printf("\n a is vowel");
+    else if(vowel=='e')
+    printf("\n e is vowel");
+    else if(vowel=='i')
+    printf("\n i is vowel");
+    else if(vowel=='o')
+    printf("\n o is vowel");
+    else if(vowel=='u')
+    printf("\n u is vowel");
+    else
+    printf("\n Sorry not a vowel");
+}
