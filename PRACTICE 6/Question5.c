e88c7395def00a18ffa83f89636d76687bbba503
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-25 21:49:50 +0530

    Add files via upload

---
+++ PRACTICE 6/Question5.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+main()
+{
+    char course;
+    printf("\nEnter the course");
+    scanf("\n%c",&course);
+    if(course=='c')
+    printf("\n You have selected c language");
+    else if(course=='p')
+    printf("\n You have selected python language");
+    else if(course=='J')
+    printf("\n You have selected Java language");
+    else
+    printf("\nSorry no other course");
+}
