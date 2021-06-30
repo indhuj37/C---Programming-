b85bbefd7115aa0efb9e6f9b82d8370b660e8fe2
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-30 12:33:56 +0530

    Add files via upload

---
+++ PRACTICE 8/Question4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+    int i=1;
+    int a=2;
+    printf("\nPrint the multiplication tables:");
+    while(i<=10)
+{
+    printf("\n%d x %d = %d",a,i,a*i);
+    i++;
+}
+}
