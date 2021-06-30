c8e65fb99b127ac0849605591c43ca31bb3a9028
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-06-30 21:46:35 +0530

    Add files via upload

--- PRACTICE 8/Question1.c
+++ PRACTICE 8/Question1.c
@@ -1,15 +1,12 @@
-//WHILE LOOP
-
-//Write the c program to print the values 10 times using while loop
 
 #include <stdio.h>
 main()
 {
-int i=1;
-printf("\nprint the values 10 times");
-while(i<=10)
-{
-printf("\n%d",i);
-i++;
-}
+    int i=1;
+    do
+    {
+    printf("\n%d",i);
+    i++;
+    }
+    while(i<=10);
 }
