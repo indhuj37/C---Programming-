bff58a28d99dcf74f43d9abf9a0be0710cc356c1
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-07-10 12:11:57 +0530

    Add files via upload

---
+++ PRACTICE 11/Question3.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+main()
+{
+    int mark[20];
+    int i;
+    int sum=0;
+    printf("\nEnter the 5 student mark");
+    for(i=1;i<=5;i++)
+    scanf("\n%d",&mark[i]);
+    printf("\nPrinting below 5 student mark");
+    for(i=5;i>=1;i--)
+    sum = sum+mark[i];
+    printf("\n%d",sum);
+}
