3ec92b7b83a61858ce426ee666a08b0e8113160c
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-07-19 13:03:45 +0530

    Add files via upload

---
+++ PRACTICE 11/Question6.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+main()
+{
+    int mark[20];
+    int i;
+    int high=0;
+    printf("\nEnter the 5 student mark");
+    for(i=1;i<=5;i++)
+    scanf("\n%d",&mark[i]);
+    printf("\nprinting 5 student highest mark");
+    for(i=1;i<=5;i++)
+    if(mark[i]>high)
+    high=mark[i];
+    printf("\n%d",high);
+}
