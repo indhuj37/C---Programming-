47336577eb4f65243d6ea4a2aa3c9fd402099fba
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-07-19 12:49:57 +0530

    Add files via upload

---
+++ PRACTICE 11/Question5.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+main()
+{
+    int mark[20];
+    int i;
+    int sum=0;
+    int avg;
+    printf("\nEnter the 5 student mark");
+    for(i=1;i<=5;i++)
+    scanf("\n%d",&mark[i]);
+    printf("\nPrinting 5 student below mark");
+    for(i=1;i<=5;i++)
+    sum=sum+mark[i];
+    avg=sum/5;
+    printf("\ntotal %d",sum);
+    printf("\naverage %d",avg);
+}
