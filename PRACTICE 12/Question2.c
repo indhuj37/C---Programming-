1127ee431304c5891fd83578b6b8bfe58f20599b
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-08-18 11:19:11 +0530

    Add files via upload

---
+++ PRACTICE 12/Question2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+{
+   int course[20][20];
+   int i;
+   printf("\nEnter the 5 student course");
+   for(i=1;i<=5;i++)
+   scanf("\n%s",course[i]);
+   printf("\nprinting the 5 student below mark");
+   for(i=1;i<=5;i++)
+   printf("\n%s",course[i]);
+}
