a1bdca91f2f0debb6422480cd05daf088a192b7e
indhuj37 <69724839+indhuj37@users.noreply.github.com>
2021-08-19 21:37:20 +0530

    Add files via upload

---
+++ PRACTICE 12/Question3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int name[20][20];
+    int i;
+    printf("\nEnter the 5 student name");
+    for(i=1;i<=5;i++)
+    scanf("\n%s",name[i]);
+    printf("\nPrinting the 5 student name below ");
+    for(i=5;i>=1;i--)
+    scanf("\n%s",name[i]);
+}
